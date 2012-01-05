0582e76fc5b047c0d7b8d602ea9a7f7d7ed03149
Jeff Risberg <jrisberg@aol.com>
2012-01-05 01:31:38 -0800

    updated port number

--- grails-app/conf/SeleniumConfig.groovy
+++ grails-app/conf/SeleniumConfig.groovy
@@ -1,10 +1,10 @@
 // Selenium-RC configuration
 
 selenium {
-	browser = "*firefox"
-	url = "http://localhost:8080/GR02"
-	screenshot {
-		dir = "./target/test-reports/screenshots"	// directory where screenshots are placed relative to project root
-		onFail = true								// true to capture screenshots on test failures
-	}
+  browser = "*firefox"
+  url = "http://localhost:9090/GR02"
+  screenshot {
+    dir = "./target/test-reports/screenshots"	// directory where screenshots are placed relative to project root
+    onFail = true								// true to capture screenshots on test failures
+  }
 }
